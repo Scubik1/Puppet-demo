a7142515495a71bc7788534da70349831c0f15e5
Scubik1 <138704511+Scubik1@users.noreply.github.com>
2023-07-20 09:50:16 +0300

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,34 @@
+node slave1.puppet {
+  package { 'httpd':
+    ensure => installed,
+  }
+  file { '/var/www/html/index.html':
+    ensure => present,
+    source => "/vagrant/index.html",
+  }
+  service { 'httpd':
+    ensure => running,
+    enable => true,
+  }
+}
+
+node slave2.puppet {
+  package { 'httpd':
+    ensure => installed,
+  }
+  package { 'php':
+    ensure => installed,
+  }
+  file { '/var/www/html/index.php':
+    ensure => present,
+    source => "/vagrant/index.php",
+  }
+  service { 'php-fpm':
+    ensure => running,
+    enable => true,
+  }
+  service { 'httpd':
+    ensure => running,
+    enable => true,
+  }
+}
